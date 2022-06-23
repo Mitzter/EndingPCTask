51467be63f7ef8d09eb787319617a04cf27d1f11
Loran Abbas <loranabbas@abv.bg>
2022-06-23 09:52:28 +0300

    Add project files.

---
+++ EndingPCTask/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EndingPCTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tasks = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] threads = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int taskToKill = int.Parse(Console.ReadLine());
+            
+            int killerThread = 0;
+
+            Stack<int> taskStack = new Stack<int>();
+            Queue<int> threadQueue = new Queue<int>();
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                taskStack.Push(tasks[i]);
+            }
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threadQueue.Enqueue(threads[i]);
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                int currentTask = taskStack.Peek();
+                int currentThread = threadQueue.Peek();
+                if (currentTask == taskToKill)
+                {
+                    killerThread = currentThread;
+                    taskStack.Pop();
+                    break;
+                }
+                if (currentThread >= currentTask)
+                {
+                    
+                    threadQueue.Dequeue();
+                    taskStack.Pop();
+                }
+                else if (currentThread < currentTask)
+                {
+                    threadQueue.Dequeue();
+                }
+
+            }
+            Console.WriteLine($"Thread with value {killerThread} killed task {taskToKill}");
+            Console.WriteLine(string.Join(" ", threadQueue));
+
+
+
+
+            
+        }
+    }
+}
